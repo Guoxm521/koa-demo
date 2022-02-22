1b2113e7c4af4968d2209cf4e45b3efd42b6dbf4
Guoxiaoming <2925891606@qq.com>
2022-02-22 21:54:40 +0800

    feat: login

--- admin-blog/src/views/login/index.vue
+++ admin-blog/src/views/login/index.vue
@@ -1,17 +1,39 @@
 <template>
-	<div>
-		<el-input v-model="input" placeholder="Please input" />
-		<el-input v-model="password" type="password" placeholder="Please input" />
-		<el-button type="primary" @click="handle">Primary</el-button>
+	<img :src="bg" class="wave" />
+	<div class="login_container">
+		<div class="img"><component :is="illustration0"></component></div>
+		<div class="login_box">321312312</div>
 	</div>
 </template>
 <script lang="ts" setup>
 	import { ref } from "vue";
-	let input = ref();
-	let password = ref();
-	let handle = () => {
-		console.log(input.value);
-		console.log(password.value);
-	};
+	import bg from "@/assets/login/bg.png";
+	import illustration0 from "@/assets/login/illustration0.svg?component";
 </script>
-<style lang='scss' scoped></style>
+<style lang='scss' scoped>
+.wave {
+	position: fixed;
+	height: 100%;
+	left: 0;
+	bottom: 0;
+	z-index: -1;
+}
+.login_container {
+	width: 100vw;
+	height: 100vh;
+	display: grid;
+	box-sizing: border-box;
+	grid-template-columns: repeat(2, 1fr);
+	grid-gap: 18rem;
+	padding: 0 2rem;
+}
+.img,
+.login_box {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+.login_box {
+	background-color: skyblue;
+}
+</style>
--- admin-blog/vite.config.ts
+++ admin-blog/vite.config.ts
@@ -3,6 +3,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
 import { resolve } from 'path'
 
 const pathResolve = (dir: string): string => {
@@ -18,11 +19,14 @@ export default defineConfig({
   resolve: {
     alias
   },
-  plugins: [vue(),
-  AutoImport({
-    resolvers: [ElementPlusResolver()],
-  }),
-  Components({
-    resolvers: [ElementPlusResolver()],
-  }),]
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    svgLoader(),
+  ]
 })
