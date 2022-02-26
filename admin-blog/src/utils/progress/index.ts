4bbc5dfddd9340869ade65ea52b04c571286687d
Guoxiaoming <2925891606@qq.com>
2022-02-26 18:49:34 +0800

    deleteold

--- admin-blog/src/utils/progress/index.ts
+++
@@ -1,9 +0,0 @@
-import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
-
-
-NProgress.configure({
-
-})
-
-export default NProgress
--- admin-blog/src/views/welcome/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div>欢迎页面</div>
-</template>
-
-<script lang="ts" setup>
-</script>
-
-<style lang="scss" scoped>
-</style>
